7ae2563847e250fbb5f356437b9d5a8b65a86625
Darkshadow-77 <156295839+Darkshadow-77@users.noreply.github.com>
2025-03-29 14:07:47 +0000

    Added a container to display

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <div class="container">
+        <h1>Welcome to our website</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.</p>
+        <a href="login.php">Login</a>
+        <a href="register.php">Register</a>
+    </div>
 </body>
 </html>
